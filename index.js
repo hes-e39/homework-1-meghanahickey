989b8617928aab3ad337d446e1445f9515a29575
Meghan Hickey <mhickey@pryoninc.com>
2024-09-16 15:01:11 -0400

    it's pretty slow but it gets the job done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 // Recommend using node-fetch for those familiar with JS fetch
 
-const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
+const COLORS = "https://nt-cdn.s3.amazonaws.com/colors.json";
 
 /**
  * @param name filter for color name
@@ -10,8 +10,42 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+const fetchColors = async ({ name, hex, compName, compHex }) => {
+  try {
+    const response = await fetch(COLORS);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    if (name) {
+      const lowerCaseName = name.toLowerCase();
+      const result = json.filter(
+        (color) => color.name.toLowerCase().includes(lowerCaseName)
+      );
+      return result;
+    } else if (hex) {
+      const result = json.filter((color) => color.hex === hex);
+      return result;
+    } else if (compName) {
+      const lowerCaseCompName = compName.toLowerCase();
+      const result = json.filter((color) =>
+        color.comp.some(
+          (compColor) => compColor.name.toLowerCase().includes(lowerCaseCompName)
+        )
+      );
+      return result;
+    } else if (compHex) {
+      const result = json.filter((color) =>
+        color.comp.some(
+          (compColor) => compColor.hex === compHex
+        )
+      );
+      return result;
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 // Leave this here
